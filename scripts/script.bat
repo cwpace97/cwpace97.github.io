c98b38258e5968651b0e7144d812ce740f145aee
cwpace97 <cwpace97@gmail.com>
2022-05-07 20:34:43 -0500

    updates to python and script

--- scripts/script.bat
+++ scripts/script.bat
@@ -1,10 +1,17 @@
-@REM run python scripts
-python "C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io\scripts\update_strava.py"
-python "C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io\scripts\update_robinhood.py"
-python "C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io\scripts\update_untappd.py"
+@echo "{{{COPYING IMAGES INTO FOLDER}}}"
+python "C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io\scripts\copy_images.py"
+@echo "================================"
 
-@REM push data to github
+@echo "{{{RUNNING PYTHON SCRIPTS}}}"
+python "C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io\scripts\update_strava.py"
+python "C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io\scripts\update_untappd.py"
+@echo "================================"
+
+@echo "{{{PUSHING DATA TO GITHUB}}}"
 cd "C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io"
 git add ".\data"
+git add ".\images"
 git commit -m "automatic data update..."
 git push
+@echo "================================"
+@echo "{{{PROCESS COMPLETE}}}"
