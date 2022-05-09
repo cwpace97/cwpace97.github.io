f56608495f57e0d8c159a54e1681823b9abed91e
cwpace97 <cwpace97@gmail.com>
2022-05-08 20:22:02 -0500

    added astro page

--- scripts/copy_images.py
+++ scripts/copy_images.py
@@ -17,11 +17,11 @@ def main(argv):
     path_to_web_dir = r'C:\Users\cwpac\OneDrive\Documents\Website\cwpace97.github.io\images\photos'
     try:
         pic_list = glob.glob(f'{path_to_astro}/*.jpg')
-        pic_list_2 = glob.glob(f'{path_to_astro}/*.jpeg')
         print(pic_list)
     except:
         print('Failed to grab photo directory, make sure hard drive is plugged in!')
 
+    #handle pictures
     for pic in pic_list:
         pic_name = pic.split("\\")[-1]
         img = Image.open(pic)
@@ -31,7 +31,8 @@ def main(argv):
         img.save(path_to_web_dir +'\\full size\\'+pic_name, "JPEG")
         img_compress.save(path_to_web_dir +'\\compressed\\'+"CMPR_"+pic_name, "JPEG")
 
-        
+    #handle videos?
+    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
